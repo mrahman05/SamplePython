b7c83b4329ef6b7bf682f1729dc5399a113641c9
Maydur Rahman <mrahman@optionmetrics.com>
2021-07-14 15:36:29 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+import os
+
+def main():
+    for i in range(0,5):
+        for j in range(0,2):
+            for k in range(10):
+                print("hello")
+
+if __name__=="__main__":
+    main()
