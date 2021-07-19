96bcd8edaa2769d304f0e930869b71f8d0659b08
Maydur Rahman <mrahman@optionmetrics.com>
2021-07-19 14:51:35 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,22 @@ import os
 def main():
     counter = 0
 	while counter <=3:
-		print("hello world")
+		print("hello")
 	counter = counter+1
+	
+	for i in range(0,counter):
+		print("world!")
+		
+	printTest()
+	print_another_test();
+	
+def printTest():
+	print("test")
+	
+
+def print_another_test():
+	print("another test")
+	
 
 if __name__=="__main__":
     main()
